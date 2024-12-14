98e62edc54a59c636f098fb1531374525359a043
Jh9925 <dkswjdgjs8902@gmail.com>
2024-12-14 22:36:41 +0900

    add em0500

---
+++ Source/ScarletNexus/Public/Character/EnemyCharacter/CommonEnemy/em0500_EnemyCharacter.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/EnemyCharacter/CommonEnemyCharacter.h"
+#include "em0500_EnemyCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SCARLETNEXUS_API Aem0500_EnemyCharacter : public ACommonEnemyCharacter
+{
+	GENERATED_BODY()
+public:
+	Aem0500_EnemyCharacter();
+
+	
+};
