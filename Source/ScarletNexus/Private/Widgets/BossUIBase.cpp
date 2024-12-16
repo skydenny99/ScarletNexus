7556b6ccc77c99c4375c97b597cca1991258ab3a
darhorang <darhorang@gmail.com>
2024-12-16 20:43:00 +0900

    Update BossUIBase

--- Source/ScarletNexus/Private/Widgets/BossUIBase.cpp
+++ Source/ScarletNexus/Private/Widgets/BossUIBase.cpp
@@ -2,27 +2,77 @@
 
 
 #include "Widgets/BossUIBase.h"
-#include "Materials/MaterialParameterCollection.h"
+#include "Kismet/KismetMaterialLibrary.h"
+#include "Components/Image.h"
+#include "PaperSprite.h"
+#include "Math/UnrealMathNeon.h"
+#include "Styling/SlateBrush.h"
 
-/*FName UBossUIBase::GetParameterName()
+/*
+#include "SlateGlobals.h"
+#include "Application/SlateApplicationBase.h"
+#include "Types/SlateVector2.h"*/
+
+void UBossUIBase::NativeOnInitialized()
 {
-	static ConstructorHelpers::FObjectFinder<UMaterialParameterCollection> MPCAsset(TEXT("/Game/Resources/UI/Enemy/Boss/Materials/MPC_Health.MPC_Health"));
-	if (MPCAsset.Succeeded())
-	{
-		MaterialParameterCollection = MPCAsset.Object;
-		TArray<FName> ParamArray = MaterialParameterCollection->GetScalarParameterNames();
+	Super::NativeOnInitialized();
+	
+	HpMaterialDynamicInstance = UKismetMaterialLibrary::CreateDynamicMaterialInstance(GetWorld(), HpMaterial);
+	Boss_HealthGauge->SetBrushFromMaterial(HpMaterialDynamicInstance);
+	HpMaterialDynamicInstance->SetScalarParameterValue("Percent",1.0f);
 
-		for ( FName ParamName : ParamArray )
-		{
-			if (ParamName != "BossHPPercent")
-			{
-				continue;
-			}
-			else
-			{
-				return ParamName;
-			}
-		}
-	}
-	return FName();
-}*/
+	StunMaterialDynamicInstance = UKismetMaterialLibrary::CreateDynamicMaterialInstance(GetWorld(), StunMaterial);
+	Boss_StunGauge->SetBrushFromMaterial(StunMaterialDynamicInstance);
+	StunMaterialDynamicInstance->SetScalarParameterValue("Percent",1.0f);
+	
+	Boss_Debuff_Icon->SetVisibility(ESlateVisibility::Collapsed);
+	
+}
+
+void UBossUIBase::UpdateHp(float Value)
+{
+	HpMaterialDynamicInstance->SetScalarParameterValue("Percent",Value);
+}
+
+void UBossUIBase::UpdateStunGauge(float Value)
+{
+	StunMaterialDynamicInstance->SetScalarParameterValue("Percent",Value);
+}
+
+//Input Enum Value
+void UBossUIBase::UpdateDebuff(int Enum)
+{
+	
+	/*
+	switch (Enum)
+	{
+	case 0:
+		
+		A.SetResourceObject(DebuffMaterial[0]);
+		Boss_Debuff_Icon->SetBrush(A);
+		break;
+	case 1:
+		//FSlateBrush B();
+		A.SetResourceObject(DebuffMaterial[1]);
+		Boss_Debuff_Icon->SetBrush(A);
+		break;
+	case 2:
+		//FSlateBrush C();
+		A.SetResourceObject(DebuffMaterial[2]);
+		Boss_Debuff_Icon->SetBrush(A);
+		break;
+	case 3:
+		//FSlateBrush D();
+		A.SetResourceObject(DebuffMaterial[3]);
+		Boss_Debuff_Icon->SetBrush(A);
+		break;
+	case 4:
+		//FSlateBrush E();
+		A.SetResourceObject(DebuffMaterial[4]);
+		Boss_Debuff_Icon->SetBrush(A);
+		break;
+	default:
+		break;
+	}*/
+}
+
--- Source/ScarletNexus/Public/Widgets/BossUIBase.h
+++ Source/ScarletNexus/Public/Widgets/BossUIBase.h
@@ -3,11 +3,15 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Materials/MaterialParameterCollection.h"
 #include "Widgets/WidgetBase.h"
 #include "BossUIBase.generated.h"
 
-class UMaterialParameterCollection;
+class UMaterialInstanceDynamic;
+class UMaterialInstance;
+class UImage;
+class UTexture2D;
+class UPaperSprite;
+struct FSlateBrush;
 
 /**
  * 
@@ -17,11 +21,48 @@ class SCARLETNEXUS_API UBossUIBase : public UWidgetBase
 {
 	GENERATED_BODY()
 
-protected:
-	UPROPERTY()
-	UMaterialParameterCollection* MaterialParameterCollection;
-
 public:
-	//FName GetParameterName();
-	//void TakeDamage(float DamageAmount);
+	//UBossUIBase();
+	
+private:
+	float CurrentHp;
+	float MaxHp;
+	//FSlateBrush A;
+	
+protected:
+	UPROPERTY(EditDefaultsOnly)
+	UMaterialInstance* HpMaterial;
+
+	UPROPERTY(EditDefaultsOnly)
+	UMaterialInstance* StunMaterial;
+	
+	UPROPERTY(EditDefaultsOnly)
+	TArray<UPaperSprite*> DebuffMaterial;
+	
+	UPROPERTY()
+	UMaterialInstanceDynamic* HpMaterialDynamicInstance;
+
+	UPROPERTY()
+	UMaterialInstanceDynamic* StunMaterialDynamicInstance;
+
+	/*UPROPERTY()
+	TArray<UPaperSprite*> DebuffMaterialDynamicInstance;*/
+	
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UImage* Boss_HealthGauge;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UImage* Boss_StunGauge;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UImage* Boss_Debuff_Icon;
+	
+public:
+	virtual void NativeOnInitialized() override;
+
+	void UpdateHp(float Value);
+	void UpdateStunGauge(float Value);
+	void UpdateDebuff(int Enum);
+	
+	void TakeDamage(float DamageAmount) { CurrentHp = (CurrentHp-DamageAmount)/MaxHp; }
 };
