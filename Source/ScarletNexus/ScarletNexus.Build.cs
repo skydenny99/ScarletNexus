d6e9850d6acf5006061819aab3d128d034ed55b6
darhorang <darhorang@gmail.com>
2024-12-16 20:43:36 +0900

    add Paper2D module

--- Source/ScarletNexus/ScarletNexus.Build.cs
+++ Source/ScarletNexus/ScarletNexus.Build.cs
@@ -8,7 +8,7 @@ public class ScarletNexus : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks", "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "AnimGraphRuntime" });
+		PublicDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks", "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "AnimGraphRuntime", "Paper2D" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
