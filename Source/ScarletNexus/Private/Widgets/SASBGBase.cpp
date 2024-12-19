cc0093861c81520fe1b1df9271fac830ff8b624e
darhorang <darhorang@gmail.com>
2024-12-19 20:58:15 +0900

    Make SAS BG Function

---
+++ Source/ScarletNexus/Private/Widgets/SASBGBase.cpp
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widgets/SASBGBase.h"
+
+#include "Components/Image.h"
+
+void USASBGBase::OnToggleCtrl()
+{
+	if (Key_L->GetColorAndOpacity() == FLinearColor::White)
+	{
+		Key_L->SetColorAndOpacity(FLinearColor::Black);
+		Deck1->SetVisibility(ESlateVisibility::Hidden);
+		DeckActive_L->SetVisibility(ESlateVisibility::Visible);
+		Deck1_Activate->SetVisibility(ESlateVisibility::Visible);
+	}
+	else
+	{
+		Key_L->SetColorAndOpacity(FLinearColor::White);
+		Deck1->SetVisibility(ESlateVisibility::Visible);
+		DeckActive_L->SetVisibility(ESlateVisibility::Hidden);
+		Deck1_Activate->SetVisibility(ESlateVisibility::Hidden);
+	}
+}
+
+void USASBGBase::OnToggleAlt()
+{
+	if (Key_R->GetColorAndOpacity() == FLinearColor::White)
+	{
+		Key_R->SetColorAndOpacity(FLinearColor::Black);
+		Deck2->SetVisibility(ESlateVisibility::Hidden);
+		DeckActive_R->SetVisibility(ESlateVisibility::Visible);
+		Deck2_Activate->SetVisibility(ESlateVisibility::Visible);
+	}
+	else
+	{
+		Key_R->SetColorAndOpacity(FLinearColor::White);
+		Deck2->SetVisibility(ESlateVisibility::Visible);
+		DeckActive_R->SetVisibility(ESlateVisibility::Hidden);
+		Deck2_Activate->SetVisibility(ESlateVisibility::Hidden);
+	}
+}
+
+void USASBGBase::OnToggleSwitch()
+{
+	if (bisSwitch == false)
+	{
+		bisSwitch = true;
+		SwitchLight->SetRenderTranslation(FVector2D(37.0f,0.0f));
+	}
+	else
+	{
+		bisSwitch = false;
+		SwitchLight->SetRenderTranslation(FVector2D(-37.0f,0.0f));
+	}
+	
+}
---
+++ Source/ScarletNexus/Public/Widgets/SASBGBase.h
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/WidgetBase.h"
+#include "SASBGBase.generated.h"
+
+class UImage;
+/**
+ * 
+ */
+UCLASS()
+class SCARLETNEXUS_API USASBGBase : public UWidgetBase
+{
+	GENERATED_BODY()
+
+private:
+	bool bisSwitch = false;
+	
+protected:
+	UPROPERTY(meta = (BindWidget))
+    UImage* Key_L;
+
+	UPROPERTY(meta = (BindWidget))
+	UImage* Deck1;
+
+	UPROPERTY(meta = (BindWidget))
+	UImage* DeckActive_L;
+
+	UPROPERTY(meta = (BindWidget))
+	UImage* Deck1_Activate;
+
+
+	UPROPERTY(meta = (BindWidget))
+	UImage* Key_R;
+
+	UPROPERTY(meta = (BindWidget))
+	UImage* Deck2;
+
+	UPROPERTY(meta = (BindWidget))
+	UImage* DeckActive_R;
+
+	UPROPERTY(meta = (BindWidget))
+	UImage* Deck2_Activate;
+
+	UPROPERTY(meta = (BindWidget))
+	UImage* SwitchLight;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void OnToggleCtrl();
+
+	UFUNCTION(BlueprintCallable)
+ 	void OnToggleAlt();
+
+	UFUNCTION(BlueprintCallable)
+	void OnToggleSwitch();
+};
