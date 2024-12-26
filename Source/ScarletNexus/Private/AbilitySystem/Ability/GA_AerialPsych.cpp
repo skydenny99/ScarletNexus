03a49220078b7943aef923c45b1c8a2dee009a1e
skydenny99 <kimdenny123@naver.com>
2024-12-26 18:39:36 +0900

    염력 공중콤보 스크립트 (ongoing)

---
+++ Source/ScarletNexus/Private/AbilitySystem/Ability/GA_AerialPsych.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/Ability/GA_AerialPsych.h"
+
+#include "Character/Character_Kasane.h"
+#include "Components/ComboSystemComponent.h"
+#include "Components/PsychokinesisComponent.h"
+#include "Utility/PsychokinesisAbilityHelper.h"
+
+bool UGA_AerialPsych::CanActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags,
+	const FGameplayTagContainer* TargetTags, FGameplayTagContainer* OptionalRelevantTags) const
+{
+	if (Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags))
+	{
+		return FPsychokinesisAbilityHelper::HasPsychokineticPropInRange(Kasane);
+	}
+	return false;
+}
+
+void UGA_AerialPsych::PreActivate(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	FOnGameplayAbilityEnded::FDelegate* OnGameplayAbilityEndedDelegate, const FGameplayEventData* TriggerEventData)
+{
+	Super::PreActivate(Handle, ActorInfo, ActivationInfo, OnGameplayAbilityEndedDelegate, TriggerEventData);
+	ComboSystem->ClearPsychComboTimer();
+	FPsychokinesisAbilityHelper::OnActivatePsychAbility(Kasane);
+}
+
+void UGA_AerialPsych::OnEndAbility(UGameplayAbility* Ability)
+{
+	Super::OnEndAbility(Ability);
+	Kasane->GetPsychokinesisComponent()->SetBlockUpdate(false);
+}
+
+void UGA_AerialPsych::ThrowProjectile()
+{
+	FPsychokinesisAbilityHelper::ActivateThrowPsychAbility(Kasane);
+}
